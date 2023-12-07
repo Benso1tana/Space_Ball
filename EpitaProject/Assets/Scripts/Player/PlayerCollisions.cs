196cadaa289ccaf5b3b7b0eb86c4050a96e8c1dd
root <nicolas.natanek@gmail.com>
2023-12-07 16:37:16 +0100

    Finish line and power up

--- EpitaProject/Assets/Scripts/Player/PlayerCollisions.cs
+++ EpitaProject/Assets/Scripts/Player/PlayerCollisions.cs
@@ -1,11 +1,38 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class PlayerCollisions : MonoBehaviour
 {
     [SerializeField] private GameObject bloodParticles;
     private Animator playerAnim;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject Panel;
+    public GameObject loseTextObject;
     
+    public GameObject[] emptyBottles;
+    public GameObject[] fullBottles;
+
+    private int countBottle = 0;
+
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+        Panel.SetActive(false);
+        loseTextObject.SetActive(false);
+        emptyBottles[countBottle].SetActive(true);
+        fullBottles[countBottle].SetActive(false);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
 
     private void Awake()
     {
@@ -23,6 +50,13 @@ public class PlayerCollisions : MonoBehaviour
                 Destroy(other.gameObject);
             });
         }
+
+        if (other.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
         if (other.tag == "Obstacle")
         {
             playerAnim.SetTrigger("kick");
@@ -39,6 +73,15 @@ public class PlayerCollisions : MonoBehaviour
         if (other.tag == "Finish")
         {
             GameEvents.instance.gameWon.SetValueAndForceNotify(true);
+            winTextObject.SetActive(true);
+            Panel.SetActive(true);
+        }
+        if (other.CompareTag("Bottle") && countBottle < emptyBottles.Length)
+        {
+            other.gameObject.SetActive(false);
+            emptyBottles[countBottle].SetActive(false);
+            fullBottles[countBottle].SetActive(true);
+            countBottle++;
         }
     }
 
